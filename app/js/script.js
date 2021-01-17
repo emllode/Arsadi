d68c7a6e05e98d948c79ce470f19b1f76c9ceebf
Christopher Bergman <cb.christopherbergman@gmail.com>
2021-01-17 10:32:40 +0100

    Fixed contact-page with contact form.

--- app/js/script.js
+++ app/js/script.js
@@ -26,3 +26,11 @@ btnHamburger.addEventListener('click', function(){
 
   }  
 });
+
+$( '.js-input' ).keyup(function() {
+  if( $(this).val() ) {
+     $(this).addClass('not-empty');
+  } else {
+     $(this).removeClass('not-empty');
+  }
+});
